be94635297675ba26d0d6a026c08ca8eada415c9
TREXAA <Dell@Hacking-6677>
2025-04-14 19:27:37 +0530

    another

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,21 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: ["https://chat-karo-rg16.onrender.com"], // ✅ your actual deployed frontend
+  credentials: true,
+}));
+
 
 app.get("/",(req,res)=>{
     res.send('hello');
 })
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 const port=process.env.PORT;
 app.use("/api/auth",usercontrol)
 app.use("/api/message",messageroute)
