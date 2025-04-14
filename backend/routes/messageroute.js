07883f7f7118a829e0b852c8a1e46a745b51d305
TREXAA <Dell@Hacking-6677>
2025-04-14 18:12:38 +0530

    another

--- backend/routes/messageroute.js
+++ backend/routes/messageroute.js
@@ -4,7 +4,7 @@ const router=express.Router()
 const{sendmessage,getmessages}=require('../controllers/messageController')
 
 router.post('/send/:id',sendmessage)
-router.get('/:id',getmessages)
+router.get('/user',getmessages)
 
 
 module.exports=router
