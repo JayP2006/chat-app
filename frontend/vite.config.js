86801be7bf19256fbc38a359d31121f2f46154b5
TREXAA <Dell@Hacking-6677>
2025-04-20 16:45:12 +0530

    another

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensure the build output directory is set to 'dist'
-  },
- 
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false
+      }
+    }
+  }
 })
