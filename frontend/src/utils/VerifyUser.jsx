d158e551d4f33e4b47baae27c1085d349d145ed7
Jay patel <116006297+JayP2006@users.noreply.github.com>
2025-08-15 14:19:22 +0530

    Delete frontend directory

--- frontend/src/utils/VerifyUser.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../Context/AuthContext';
-
-export const VerifyUser = () => {
-  const { authUser } = useAuth();
-  return authUser ? <Outlet /> : <Navigate to="/login" />;
-};
