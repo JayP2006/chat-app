2dad1dab83a07da26e01725c0f32e5655e6e9847
Jay patel <116006297+JayP2006@users.noreply.github.com>
2025-08-15 14:19:07 +0530

    Delete backend directory

--- backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const isLogin = require('../middleware/isLogin')
-const { getUserBySearch, getCurrentChatters } = require('../controllers/userHandler')
-const router=express.Router()
-
-router.get('/search',getUserBySearch)
-router.get('/currentchatters',getCurrentChatters)
-
-module.exports=router
